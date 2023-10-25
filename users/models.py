c7946101c6f1da98606fefaf286bd7cd4543e836
Chesed Kim <starmireu@gmail.com>
2023-10-25 17:25:36 +0900

    ♻️ refactor : 코드 리팩터링 - users/tests 중심

--- users/models.py
+++ users/models.py
@@ -1,44 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.core.validators import BaseValidator
-from django.core.exceptions import ValidationError
-
-# MIME 타입 검사를 위한 라이브러리 (이미지 사진 업로드)
-import magic
-
-
-class MimeTypeValidator:
-    def __init__(self, mimetypes):
-        self.mimetypes = mimetypes
-
-    def __call__(self, value):
-        if not value:
-            return
-        try:
-            mime = magic.Magic(mime=True)
-            file_mime = mime.from_buffer(value.read())
-            if file_mime not in self.mimetypes:
-                raise ValidationError(
-                    f"Invalid file type. Allowed types are: {', '.join(self.mimetypes)}"
-                )
-        except Exception as e:
-            raise ValidationError(f"Error reading file: {e}")
-        finally:
-            value.seek(0)
-
-
-# 이미지 검증기를 클래스로 변경
-class ImageSizeValidator(BaseValidator):
-    message = "너무 큽니다...2MB 이하로 사진 용량을 줄여주세요."
-
-    def __init__(self, limit_value=2 * 1024 * 1024, **kwargs):  # 2MB의 기본값 추가
-        super().__init__(limit_value, **kwargs)  # 부모 클래스의 __init__ 호출
-
-    def compare(self, a, b):
-        return a > b
-
-    def clean(self, x):
-        return x.file.size
 
 
 class UserManager(BaseUserManager):
@@ -63,10 +24,6 @@ class User(AbstractBaseUser):
     previous_password = models.CharField(max_length=50, blank=True, null=True)
     profile_picture = models.ImageField(
         upload_to="profile_pics/",
-        validators=[
-            ImageSizeValidator(),
-            MimeTypeValidator(["image/jpeg", "image/png"]),
-        ],
         blank=True,
         null=True,
     )
@@ -85,8 +42,4 @@ class User(AbstractBaseUser):
         return self.username.split()[0] if self.username else self.email.split("@")[0]
 
     def save(self, *args, **kwargs):
-        if self.profile_picture:
-            # 수동으로 실행하도록 변경 (마이그레이션 직렬화 문제)
-            validator = MimeTypeValidator(["image/jpeg", "image/png"])
-            validator(self.profile_picture)
         super().save(*args, **kwargs)
--- users/tests.py
+++ users/tests.py
@@ -1,9 +1,4 @@
 from django.test import TestCase
-from django.core.files.uploadedfile import SimpleUploadedFile
-from django.core.exceptions import ValidationError
-from django.urls import reverse
-import os
-
 from .models import User
 
 
@@ -15,41 +10,11 @@ class UserModelTestCase(TestCase):
         self.user.set_password("testpass")
         self.user.save()
         self.client.force_login(self.user)
-        self.mock_file = SimpleUploadedFile(
-            "testfile.txt", b"file_content", content_type="text/plain"
-        )
-
-    def test_file_upload(self):
-        mock_image_content = b"\xFF\xD8\xFF\xE0" + b"0" * 508 + b"\xFF\xD9"
-        self.mock_file = SimpleUploadedFile(
-            "testfile.jpg", mock_image_content, content_type="image/jpeg"
-        )
-
-        response = self.client.post(
-            reverse("user-list"),
-            {
-                "username": "newtestuser",
-                "email": "newtestuser@example.com",
-                "profile_picture": self.mock_file,
-            },
-        )
-        self.assertEqual(response.status_code, 200)
-        instance = User.objects.get(profile_picture="testfile.jpg")
-        self.assertTrue(instance.profile_picture)
 
     def test_user_creation(self):
         self.assertEqual(self.user.username, "testuser")
         self.assertEqual(self.user.email, "testuser@example.com")
         self.assertTrue(self.user.check_password("testpass"))
-
-    def test_user_should_validate_image_size(self):
-        large_file_content = os.urandom(3 * 1024 * 1024)
-        large_file = SimpleUploadedFile(
-            "large_image.jpg", large_file_content, content_type="image/jpeg"
-        )
-        self.user.profile_picture = large_file
-        with self.assertRaises(ValidationError):
-            self.user.full_clean()
 
     def test_user_should_return_username(self):
         self.assertEqual(self.user.get_full_name(), "testuser")
