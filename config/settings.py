c42346fca1e4db65653d802d9d5a9063baa59d91
kyeon06 <jkyeon06@gmail.com>
2023-10-25 20:35:25 +0900

    :sparkles: feat : pagination 기능 추가

--- config/settings.py
+++ config/settings.py
@@ -144,5 +144,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ],
-
+    
+    'DEFAULT_PAGINATION_CLASS': 'boards.pagination.CustomPageNumberPagination',
+    'PAGE_SIZE': 10
 }
