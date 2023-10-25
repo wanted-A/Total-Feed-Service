9424440daf032427841b43e10e1a005a1c53a673
Chesed Kim <starmireu@gmail.com>
2023-10-25 18:07:15 +0900

    🐛 fix : swagger 관련 drf-yasg 스키마 수동적용

--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,8 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ("username", "email", "profile_picture")
+        fields = ("username", "email", "password")
--- users/views.py
+++ users/views.py
@@ -1,17 +1,21 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 
 from .models import User
 from .serializers import UserSerializer
 
 
 class UserView(APIView):
+    permission_classes = [permissions.AllowAny]
+
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
 
+    @swagger_auto_schema(request_body=UserSerializer)
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
