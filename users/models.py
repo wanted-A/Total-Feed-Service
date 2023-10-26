8e1599879407e662f2da819d8ce8aaea3217dfe2
Chesed Kim <starmireu@gmail.com>
2023-10-26 09:44:33 +0900

    🐛 fix : admin 계정 생성 로직 수정

--- users/models.py
+++ users/models.py
@@ -40,6 +40,8 @@ class User(AbstractBaseUser):
     )
     is_approved = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
 
