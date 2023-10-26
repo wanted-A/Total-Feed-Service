f1e8f7c62088bde15334b256eb06334e93920226
Chesed Kim <starmireu@gmail.com>
2023-10-26 09:55:14 +0900

    ♻️ refactor: users: PermissionsMixin 추가

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.contrib.auth.hashers import make_password, check_password
 
 
@@ -28,7 +32,7 @@ class PreviousPassword(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=20, unique=True, blank=False)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
