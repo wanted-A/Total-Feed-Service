8cc3fe6a88f2563f56d60ee3d3c3d3675a31e0b8
ssu-uky <id_suhyun@naver.com>
2023-10-27 12:07:47 +0900

    Revert ":sparkles: feat : BoarddetailView 의 조회수(viewcount) 추가"
    This reverts commit 6351f9a8a32bd4661071ed97bbb387c1e250522c.

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from .models import User
-
 
 # Register your models here.
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        "pk",
-        "username",
-        "email",
-    )
