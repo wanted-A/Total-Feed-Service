2577c8ae53be550013b29dc97f3b86e964515f7e
kyeon06 <jkyeon06@gmail.com>
2023-10-30 18:26:28 +0900

    :bulb: chore : 클래스명 변경(Boards->Board)

--- boards/urls.py
+++ boards/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import BoardShareAPIView, BoardsListAPIView, BoardDetailView, AnalyticsView, BoardWriteView, BoardLikesView
+from .views import BoardShareAPIView, BoardListAPIView, BoardDetailView, AnalyticsView, BoardWriteView, BoardLikesView
 
 
 urlpatterns = [
-    path("", BoardsListAPIView.as_view(), name="boards-list"), # 게시글 목록 조회 API
+    path("", BoardListAPIView.as_view(), name="boards-list"), # 게시글 목록 조회 API
     path("write/", BoardWriteView.as_view(), name="board-write"), # 게시글 작성 API
     path("<int:content_id>/", BoardDetailView.as_view(), name="board-detail"), # 게시글 상세 조회 API
     path("<str:feed_type>/likes/<int:content_id>/", BoardLikesView.as_view(), name="board-likes"), # 게시글 작성 API
